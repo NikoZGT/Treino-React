207a414cdc84619cfa6e482ff2c488e220e6224c
NicolasZago <59876954+NicolasZago@users.noreply.github.com>
2023-01-20 15:12:43 -0300

    Frok repository
    Nikolas

--- Com React/src/Components/Banner/index.jsx
+++ Com React/src/Components/Banner/index.jsx
@@ -8,7 +8,7 @@ export default function Banner() {
             <div class="nz-container">
                 <div class="nz-banner__img">
                     <div class="nz-logo"></div>
-                    <h1>Explore the best food</h1>
+                    <h1>Explore the best food teste</h1>
                     <input className="navbarinput" type="text" placeholder="Specify your taste" />
                 </div>
             </div>
